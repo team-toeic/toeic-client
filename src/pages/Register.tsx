b8c27320541605b9e4ae42a4dd8c216823e0e292
leemirae <416homin@daum.net>
2024-01-07 15:14:45 +0900

    workflow test 3

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { RegisterCSS } from "../style/pages/RegisterCSS";
-import Logo from "../components/common/Logo";
-import Mascort from "../components/common/Mascort";
 
 function Register() {
   return <RegisterCSS>회원가입</RegisterCSS>;
