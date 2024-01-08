16aa01f8636fd129a70158c8e01c7fb5d9a01115
leemirae <416homin@daum.net>
2024-01-08 23:37:19 +0900

    workflow test 5

---
+++ src/style/components/HeaderCSS.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HeaderCSS = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 20px;
+`;
