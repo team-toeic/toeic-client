5922aa1ea26e8435396042e35baa99deb11f4e1c
leemirae <416homin@daum.net>
2024-01-09 21:21:36 +0900

    feat : sample #7

--- src/components/register/Agreement.tsx
+++ src/components/register/Agreement.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AgreementCSS } from "../../style/components/register/AgreementCSS";
 
 function Agreement() {
-  return <AgreementCSS>커밋 컨벤션 테스트</AgreementCSS>;
+  return <AgreementCSS>커밋 컨벤션 테스트2</AgreementCSS>;
 }
 
 export default Agreement;
